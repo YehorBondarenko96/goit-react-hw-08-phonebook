8c1685e64313a8bc1aa84cdc6b1020ba99e0f7c9
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-17 18:50:22 +0200

    1.0.0-17.01.24

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,34 @@
 import css from '../Styles.module.css';
 import { ItemContact } from 'components/ItemContact/ItemContact';
 import { selectContacts, selectFilter, selectIsLoading, selectError  } from '../../redux/selectors';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchContacts } from "../../redux/opertions";
 import { useDispatch, useSelector } from "react-redux";
 import { Loader } from "../Loader/Loader";
+import { searchForBackground } from 'services/searchForStyles';
 
 export const ContactList = () => {
     let contacts = useSelector(selectContacts);
     const filter = useSelector(selectFilter);
+    const [backgroundImg, setBackgroundImg] = useState(null);
 
-
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const isLoading = useSelector(selectIsLoading);
     const error = useSelector(selectError);
 
     useEffect(() => {
-        dispatch(fetchContacts());
+        const fetchData = async () => {
+            dispatch(fetchContacts());
+    
+            try {
+                const [allInfoBackgroundImg] = await searchForBackground();
+                setBackgroundImg(allInfoBackgroundImg.largeImageURL);
+            } catch (error) {
+                console.error('Error fetching background image:', error);
+            }
+        };
+    
+        fetchData();
     }, [dispatch]);
 
     if(filter.length > 0) {
@@ -24,7 +36,7 @@ export const ContactList = () => {
             }
 
     return(
-        <div className={css.divForContactList}>
+        <div className={css.divForContactList} style={{background: `url(${backgroundImg})`}}>
             {error && <h2>Oopsss...Something went wrong...</h2>}
             {isLoading && !error ? <Loader /> : (
                 <ul className={css.listContacts}>
