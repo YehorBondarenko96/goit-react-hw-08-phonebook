af23bbebb71218b396b66f7f2aa93316da7f8907
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-18 14:49:19 +0200

    1.4.0-18.01.24

--- src/components/ItemContact/ItemContact.jsx
+++ src/components/ItemContact/ItemContact.jsx
@@ -1,9 +1,42 @@
 import css from '../Styles.module.css';
-import {  useDispatch } from 'react-redux';
+import {  useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/opertions';
+import { selectBackgroundImages } from '../../redux/selectors';
+import { useState, useEffect } from 'react';
 
 export const ItemContact = ({contact}) => {
     const dispatch = useDispatch();
+    const images = useSelector(selectBackgroundImages);
+    const [numbsImg, setNumbsImg] = useState([]);
+    const [img, setImg] = useState(null);
+
+    useEffect(() => {
+    const randomInt = () => {
+        const lengthImages = images.length;
+    
+        if (lengthImages !== 0){
+            const newNumb = Math.floor(Math.random() * lengthImages) + 1;
+        
+            if(numbsImg.includes(newNumb)){
+                randomInt(lengthImages)
+            } else if (numbsImg.length >= 5){
+                const newNumbsImg = numbsImg.splice(0, 1);
+                setNumbsImg([...newNumbsImg, newNumb])
+            } else {
+                setNumbsImg([...numbsImg, newNumb])
+            };
+    }
+    };
+
+    const backgroundImg = () => {
+        randomInt();
+        const thisNumb = numbsImg[numbsImg.length - 1];
+
+        setImg(images[thisNumb]);
+    };
+    
+        backgroundImg()
+    }, [images])
 
     const updateStateForDelete = () => {
         const idContact = contact.id;
@@ -11,7 +44,12 @@ export const ItemContact = ({contact}) => {
         };
 
     return(
-        <li key={contact.id} className={css.itemContscts}>
+        <li key={contact.id} className={css.itemContscts}
+        style={{
+            backgroundImage: `url(${img})`,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            }}>
                 <h2 className={css.pItemContactsName}>{contact.name}:</h2> 
                 <p className={css.pItemContactsNumber}>{contact.phone}</p>
                 <button id={contact.id} className={css.buttonDelete} type='button' onClick={updateStateForDelete}>
