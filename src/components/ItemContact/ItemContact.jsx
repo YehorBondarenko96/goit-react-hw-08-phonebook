4d141d7189e619620fd30f35793b14168d9177cf
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-19 11:48:15 +0200

    1.8.3-19.01.24

--- src/components/ItemContact/ItemContact.jsx
+++ src/components/ItemContact/ItemContact.jsx
@@ -1,11 +1,11 @@
 import css from '../Styles.module.css';
-import {  useDispatch, useSelector } from 'react-redux';
+import {  useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/opertions';
-import { selectNumbsForImg } from '../../redux/selectors';
+// import { selectNumbsForImg } from '../../redux/selectors';
 
 export const ItemContact = ({contact, imagesForContacts, index}) => {
     const dispatch = useDispatch();
-    const numbsForImg = useSelector(selectNumbsForImg);
+    // const numbsForImg = useSelector(selectNumbsForImg);
 
     const updateStateForDelete = () => {
         const idContact = contact.id;
@@ -13,21 +13,20 @@ export const ItemContact = ({contact, imagesForContacts, index}) => {
         };
 
     const reservedImg = imagesForContacts[0];
-    const secondImagesForContacts = imagesForContacts.filter(img => img !== reservedImg);
+    // const secondImagesForContacts = imagesForContacts.filter(img => img !== reservedImg);
 
-    const lengthNumbsForImg = numbsForImg.length - 1;
-    let numb = 1;
+    // const lengthNumbsForImg = numbsForImg.length - 1;
+    // let numb = 1;
 
-    if (index > lengthNumbsForImg){
-        numb = numbsForImg[index % lengthNumbsForImg];
-    } else {
-        numb = numbsForImg[Math.abs(index)];
-    }
+    // if (index > lengthNumbsForImg){
+    //     numb = numbsForImg[index % lengthNumbsForImg];
+    // } else {
+    //     numb = numbsForImg[Math.abs(index)];
+    // }
 
     
-    const img = secondImagesForContacts[numb];
-    console.log('numb: ', numb);
-    console.log('img: ', img);
+    // const img = secondImagesForContacts[numb];
+    const img = 'https://pixabay.com/get/g3ed7d486cc5b37731e8ed71396efa6382fca2f5612edc7fa4e1432973c87e1a5de6c13079cfcaf6225e626de69f10f90_1280.jpg';
 
     return(
         <li key={contact.id} className={css.itemContscts}
