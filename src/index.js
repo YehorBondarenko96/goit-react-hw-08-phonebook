b5a439c53417d95a8068504bd71762d3ae0e3ce4
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-25 15:18:07 +0200

    3.0.1-25.01.24

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
