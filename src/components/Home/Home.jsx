3f539c7e8f22a777c24f8e5cde03b3140051fced
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-02-05 15:24:30 +0200

    10.3.0-05.02.24

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,20 +8,26 @@ const Home = () => {
     return(
         <div className={css.allDivHome}>
         <div className={css.greetingHome}>
-        <h2 className={css.hllGreetingHome}>Hello, in </h2><span className={css.logoNameGreetingHome}>MyPhoneBook</span>
+            <h1 className={css.hlHome}>ContactHub</h1>
+            <svg className={css.logoHome}>
+            <use href="../contacts-svgrepo-com.svg#ContactsBook"></use>
+            </svg>
+        <h2 className={css.hllGreetingHome}>Welcome to </h2>
+        <span className={css.logoNameGreetingHome}>ContactHub</span>
+        <p className={css.sloganHome}>Find easily, Store conveniently</p>
         </div>
         <div className={css.buttonsHome}>
         {isLoggedIn ? (
             <Link to={'/contacts'}>
-                <button className={css.buttonHome}>Log In in <span className={css.logoNameBtnHome}>MyPhoneBook</span></button>
+                <button className={css.buttonHome}>Log in <span className={css.logoNameBtnHome}>ContactHub</span></button>
             </Link>
         ) : (
             <>
             <Link to={'/login'}>
-            <button className={css.buttonHome}>Log in <span className={css.logoNameBtnHome}>MyPhoneBook</span></button>
+            <button className={css.buttonHome}>Log in <span className={css.logoNameBtnHome}>ContactHub</span></button>
             </Link>
             <Link to={'/register'}>
-            <button className={css.buttonHome}>Sign in <span className={css.logoNameBtnHome}>MyPhoneBook</span></button>
+            <button className={css.buttonHome}>Sign in <span className={css.logoNameBtnHome}>ContactHub</span></button>
             </Link>
             </>
         )}
